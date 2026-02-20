20e67b0e2bf6e3334fca480a0a2ccd54c704b8a0
dgarciah98 <dieg0farrobaterra@gmail.com>
2026-02-20 01:41:07 +0100

    feat: add logo

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -2,13 +2,18 @@ import { pathToRoot } from "../util/path"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
 import { i18n } from "../i18n"
+import { joinSegments } from "../util/path"
 
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
+  const iconPath = joinSegments(baseDir, "static/logo.png")
   return (
     <h2 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>{title}</a>
+	   <a href={baseDir}>
+		  <img class="logo" src={iconPath} alt="goodvibes" />
+	      {title}
+	   </a>
     </h2>
   )
 }
@@ -18,6 +23,13 @@ PageTitle.css = `
   font-size: 1.75rem;
   margin: 0;
   font-family: var(--titleFont);
+}
+.logo {
+max-heigh: 200px;
+min-height: 50px;
+max-width: 200px;
+min-width: 50px;
+margin: 0;
 }
 `
 
